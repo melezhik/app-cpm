f3acf0e6a3a7d87b6ade743dd9eb880e5a4d1c7c
Alexey Melezhik <melezhik@gmail.com>
2016-08-22 14:40:23 +0300

    verbose

--- modules/cpm/story.bash
+++ modules/cpm/story.bash
@@ -8,10 +8,10 @@ export https_proxy=$(config https_proxy)
 
 if test -z $install_base; then
     echo install $package ...
-    cpm install `-z $verbose && echo -v` -g $package || exit 1
+    cpm install `test -z $verbose && echo -v` -g $package || exit 1
 else
     echo install $package into $install_base ...
-    cpm install `-z $verbose && echo -v` -L $install_base  $package || exit 1
+    cpm install `test -z $verbose && echo -v` -L $install_base  $package || exit 1
 fi
 
 echo 'install ok'

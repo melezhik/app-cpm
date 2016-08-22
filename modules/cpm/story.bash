6baa6429c3136f1a515c0a4169e65077d37ff27b
Alexey Melezhik <melezhik@gmail.com>
2016-08-22 14:39:34 +0300

    prototype

--- modules/cpm/story.bash
+++ modules/cpm/story.bash
@@ -1,15 +1,17 @@
 install_base=$(config install-base)
+verbose=$(config verbose)
 package=$(story_var package)
+
 
 export http_proxy=$(config http_proxy)
 export https_proxy=$(config https_proxy)
 
 if test -z $install_base; then
     echo install $package ...
-    cpm install -g $package || exit 1
+    cpm install `-z $verbose && echo -v` -g $package || exit 1
 else
     echo install $package into $install_base ...
-    cpm install -L $install_base  $package || exit 1
+    cpm install `-z $verbose && echo -v` -L $install_base  $package || exit 1
 fi
 
 echo 'install ok'
